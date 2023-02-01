d1e0c8b5b591816d7b673a9cfe0ee14a30bc2ca2
Grayson <Grayson_Arcemont1@baylor.edu>
2023-02-01 14:01:22 -0600

    person dictionaries complete

--- 2_person_dict.py
+++ 2_person_dict.py
@@ -5,3 +5,17 @@ person["age"] = 51
 person["spouse"] = "Edna"
 person["children"] = ["Ralph", "Betty", "Joey"]
 person["pets"] = {"dog": "Fido", "cat": "Sox"}
+
+# name of second child
+print(person.get["children"][1])
+
+# name of cat
+print(type(person["pets"]))
+
+# use a loop to list each child
+for i in person["children"]:
+    print(i)
+
+# type of pet: dog name: Fiao
+for i, j in person["pets"].items():
+    print(f"Type of pet: {i} name of pet: {j}")
